2d5b988d509b8615f1f3cb9498696ce58583c731
wutl92 <wutl92@aliyun.com>
2021-03-01 16:27:31 +0800

    装饰者模式

--- src/main/java/com/wutl/demo/sjms/zszms/Beverage.java
+++ src/main/java/com/wutl/demo/sjms/zszms/Beverage.java
@@ -4,9 +4,8 @@ package com.wutl.demo.sjms.zszms;
  * @author wutl
  * @Title: 饮料超类
  * @Package
- * @Description: 装饰者模式
+ * @Description: 装饰者模式 (组合)
  * 动态地将责任附加到对象上。若要扩展功能，装饰者提供了比继承更有弹性的替代方案。
- * (组合)
  * @date 2021-3-1 15:50
  */
 public abstract class Beverage {
